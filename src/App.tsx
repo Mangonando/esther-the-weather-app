a9126abfdd31434844252a039646c09165c2c6c5
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2023-05-23 18:14:34 +0200

    API call. useEffect structure

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ import './App.css'
 export default function App() {
   const [weatherData, setWeatherDate] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    const getWeatherData = async () => {
+      try {
+        //something
+      } catch {
+        //something else
+      }
+    }
+    getWeatherData()
+  })
 
   return <div className="i-box">🌬 💨 💨 Esther</div>
 }
