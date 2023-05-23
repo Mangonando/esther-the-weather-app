f1fae5ff41c193fe165cb9aaccf5a8399e85e8c8
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2023-05-23 18:12:00 +0200

    API call structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
 export default function App() {
+  const [weatherData, setWeatherDate] = useState(null)
+
+  useEffect(() => {})
+
   return <div className="i-box">🌬 💨 💨 Esther</div>
 }
