a046d399de8b8f56a85a8830bc5ab265d9c375e0
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2023-05-26 12:09:42 +0200

    Create new color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   darkMode: false,
   theme: {
     extend: {},
+    colors: {
+      retro: {
+        blue: '#064C72',
+        cream: '#ECAE7D',
+        green: {
+          DEFAULT: '#8DB4AD',
+          dark: '#026C80',
+        },
+        orange: '#ED6335',
+        red: '#E9311A',
+      },
+    },
   },
   variants: {
     extend: {},
