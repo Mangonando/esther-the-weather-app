1d611ea10e7b7edda6722094123e3ef65eb3cc8c
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2023-05-29 11:19:03 +0200

    Add styling to weather component

--- src/components/Weather/Weather.tsx
+++ src/components/Weather/Weather.tsx
@@ -32,16 +32,29 @@ export const Weather: React.FC<WeatherProps> = ({ weatherData }) => {
     <>
       <div className="weather-container component-padding">
         <div className="temperature-container">
-          <div className="temperature-number">{weatherData.main.temp}</div>
-          <div className="temperature-symbol">&nbsp;c</div>
+          <div className="temperature-number">
+            {Math.floor(weatherData.main.temp)}
+          </div>
+          <div className="temperature-symbol">c</div>
         </div>
-        <div>
+        <div className="location-name">
           {weatherData.name}, {weatherData.sys.country}
         </div>
-        <div>{weatherData.weather[0].main}</div>
-        <div>Feels like: {weatherData.main.feels_like}c</div>
-        <div>Temp min: {weatherData.main.temp_min}c</div>
-        <div>Temp max: {weatherData.main.temp_max}c</div>
+        <div className="weather-condition">{weatherData.weather[0].main}</div>
+        <div className="additional-temperature-container">
+          <div className="additional-temperature-box">
+            <div>{Math.floor(weatherData.main.feels_like)}c</div>
+            <div>Feels like</div>
+          </div>
+          <div className="additional-temperature-box">
+            <div>{Math.floor(weatherData.main.temp_min)}c</div>
+            <div>Temp min</div>
+          </div>
+          <div className="additional-temperature-box">
+            <div>{Math.floor(weatherData.main.temp_max)}c</div>
+            <div>Temp max</div>
+          </div>
+        </div>
       </div>
     </>
   )
